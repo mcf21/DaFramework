4f09689a044bf7bc2911307e295491a8a5a68e50
marcel <marcel.mcfall@artprocessors.net>
2020-10-22 01:36:45 +1100

    Initial Commit

---
+++ DaFramework/DaFramework.h
@@ -0,0 +1,18 @@
+//
+//  DaFramework.h
+//  DaFramework
+//
+//  Created by Marcel McFall on 22/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DaFramework.
+FOUNDATION_EXPORT double DaFrameworkVersionNumber;
+
+//! Project version string for DaFramework.
+FOUNDATION_EXPORT const unsigned char DaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DaFramework/PublicHeader.h>
+
+
